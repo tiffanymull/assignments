8b1e47a5ca423bada004f981b9ce29062689724f
Tiffany Mull <tgwenmull@gmail.com>
2019-02-08 13:35:55 -0700

    array sort exercise

--- exercises/Colossal_Adventure_Console_RPG/app.js
+++ exercises/Colossal_Adventure_Console_RPG/app.js
@@ -1,1 +1,12 @@
 var readline = require('readline-sync')
+
+var isAlive = true;
+
+var player = {
+    name:
+    health: 100
+    inventory: []
+}
+
+
+var readline
--- exercises/array_sort_exercises/app.js
+++ exercises/array_sort_exercises/app.js
@@ -13,7 +13,8 @@
 // var numbers = [17, 42, 6, 103, 5, 2]
 
 // numbers.sort(function(a, b){
-//     return a < b
+//     // return a < b
+//     return b - a      //<--both work
 // })
 // console.log(numbers)
 
@@ -21,7 +22,40 @@
 // var food = ["cheese", "beef", "carrots", "coddled eggs"]
 
 // food.sort(function(a, b){
-//     return a - b
-// })
+//     return a.length - b.length
+// });
+// console.log(food);
 
+//4. Sort an array alphabetically
+// var animalNickNames = ["rhino", "pup", "birdie", "kitty", "horsey"]
+//     animalNickNames.sort();
 
+// console.log(animalNickNames);
+
+//5. Sort the objects in the array by age.
+
+var characters = [
+    {
+        name: "Quiet Samurai",
+        age: 22
+    },
+    {
+        name: "Arrogant Ambassador",
+        age: 100
+    },
+    {
+        name: "Misunderstood Observer",
+        age: 2
+    },
+    {
+        name: "Unlucky Swami",
+        age: 77
+    }
+]
+
+characters.sort(function(a, b){
+
+    return parseInt(a.age) - parseInt(b.age);
+});
+
+console.log(characters)
