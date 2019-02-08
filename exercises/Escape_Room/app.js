bd60705a934311547928fea847a4840158149b62
Tiffany Mull <tgwenmull@gmail.com>
2019-02-08 01:03:58 -0700

    Escape Room

--- exercises/Escape_Room/app.js
+++ exercises/Escape_Room/app.js
@@ -7,6 +7,21 @@ var readline = require('readline-sync');
 var isAlive = true;
 var hasKey = false;
 
+//Could make a series of functions up here for what you want to happen and call on them as you go through the story.
+
+// function lookForKey(){
+//     var lookWhere = readline.question('You see a [loose floorboard], a [hanging lamp], [box of cigarettes], and a [jar of change]. Type the place you want to look first exactly as it is written inside the brackets. ')
+//                 if (lookWhere === 'loose floorboard') {
+//                 console.log('You pry up the floorboard. All you see is a spider and its web. Where do you look next? ')   //<---I need to get this to loop back up into that question.
+//                 } else if (lookWhere === 'hanging lamp') {
+//                 console.log('You tip the hanging lamp upside down. It\'s hot and dusty, but there\'s nothing there. Where do you look next?') 
+//                 } else if (lookWhere === 'box of cigarettes') {
+//                 console.log('You tear open the cigarette box. All that\'s inside are two withered gaspers. No key. Where do you look next?' )
+//                 } else if (lookWhere === 'jar of change') {
+//                     hasKey = true
+//                 console.log('You dump the change on the floor and paw through it. There\'s a key!')
+// }
+//write out story in functions and then below them, call them in the order I want them to happen.
 //1.
 var playerName = readline.question('What\'s your name?')
     console.log('Hi, ' + playerName + ', you\'re in trouble. ')
@@ -17,15 +32,19 @@ while(isAlive){
     if (handHole === 'Yes'){
         isAlive = false
         console.log('The tiger snake that lives in the hole bit your hand and you died. Game over. ')   //<-- I need to get this to just stop and not keep going, since they died.
+        break; //<-- not necessarily needed bc isAlive = false takes care of it.
     } else if (handHole === 'No'){
         console.log('You kept your hand out of the hole. You are still alive.')
+        break;
     } else {
         console.log('You must type either Yes or No.')
-    }
+    }}
 //3.
+while(!hasKey){  //<-- same as saying while (hasKey === false)
     var lookOrNot = readline.question('The door to the room is locked. There may be a key somewhere. Will you look for it? Type Yes or No.')
     if (lookOrNot === 'Yes'){
-        console.log('Good idea. ')
+        console.log('Good idea. ')    //<-- could've put var lookWhere into a function called lookWhere or whatever
+
 //4.
             var lookWhere = readline.question('You see a [loose floorboard], a [hanging lamp], [box of cigarettes], and a [jar of change]. Type the place you want to look first exactly as it is written inside the brackets. ')
                 if (lookWhere === 'loose floorboard') {
@@ -38,23 +57,26 @@ while(isAlive){
                     hasKey = true
                 console.log('You dump the change on the floor and paw through it. There\'s a key!')
 //5.
+              var indecisive = true;      
+                while(indecisive === true) {    
                     var openDoor = readline.question('Now that you have the key, are you going to try the door? Type Yes or No.')
                         if (openDoor === 'Yes') {
                         console.log('You try the key. It worked! The door opened! Unfortunately, there is a murderous clown on the other side. Have fun!')
-                        } else {
-                        console.log('Though you have the key, you are too scared to try the door. You opt to starve to death rather than see what\'s on the other side.') //<--make it loop back or kill them
-                        }
+                            indecisive = false;  //<-- could've used break;
+                        } else if (openDoor === 'No'){
+                        console.log('Though you have the key, you are too scared to try the door. You wet yourself. And died.') //<--make it loop back or kill them
+                            indecisive = false;
 
                         } else {
                         console.log('Invalid. Please type one of the options next time.')
                         }
-
+                    }
     } else if (lookOrNot === 'No'){
-    console.log('Rather than look for a key, you stand ther staring. Nothing happens. You\'re getting pretty thirsty. ')  //<-- I need to get this to loop back up into the question
+    console.log('Rather than look for a key, you stand there staring. Nothing happens. You\'re getting pretty thirsty. ')  //<-- I need to get this to loop back up into the question
     } else {
     console.log('You must type either Yes or No.')    //<-- I need to get this to loop back up into the question
     }
-
+}
 }
 
 
---
+++ exercises/array_sort_exercises/app.js
@@ -0,0 +1,27 @@
+
+//1. Sort an array from smallest number to largest.
+// var numbers = [17, 42, 6, 103, 5, 2]
+
+//     numbers.sort(function(a, b){
+//         // return a - b        //<-- either works
+//         return a > b          // <-- either works
+//     })
+
+// console.log(numbers)
+
+//2. Sort an array from largest number to smallest.
+// var numbers = [17, 42, 6, 103, 5, 2]
+
+// numbers.sort(function(a, b){
+//     return a < b
+// })
+// console.log(numbers)
+
+//3. Sort an array from shortest string to longest.
+// var food = ["cheese", "beef", "carrots", "coddled eggs"]
+
+// food.sort(function(a, b){
+//     return a - b
+// })
+
+
