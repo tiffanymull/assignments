6ff74ff2a0d7cdd782751da0af02b4d11080c13f
Tiffany Mull <tgwenmull@gmail.com>
2019-02-11 16:10:37 -0700

    working

---
+++ exercises/JSONs_Deli/app.js
@@ -0,0 +1,76 @@
+//appetizers
+
+"appetizers": [{
+    "dish": "chili fries",
+    "ingredients": ["chili", "fries"],
+    "description": "Greasy fries with chili."
+},
+{
+    "dish": "nachos",
+    "ingredients": ["tortilla chips", "cheese"],
+    "description": "Crunchy, cheesy nachos."
+},
+{
+    "dish": "bruschetta",
+    "ingredients": ["grilled bread", "olive oil", "salt"],
+    "description": "Crunchy, flavored bread."
+},
+] }
+
+//cold sandwiches
+"cold sandwiches": [{
+    "sandwich": "egg salad",
+    "ingredients": ["egg", "mayonnaise", "mustard"],
+    "description": "Sliced up boiled eggs with garnish."
+},
+{
+    "sandwich": "BLT",
+    "ingredients": ["bacon", "lettuce", "tomato"],
+    "description": "Crunchy pig sandwich."
+}
+
+]
+
+//hot sandwiches
+"hot sandwiches": [{
+    "sandwich": "grilled cheese",
+    "ingredients": ["cheese", "pepper"],
+    "description": "Hot cheese between bread."
+},
+{
+    "sandwich": "french dip",
+    "ingredients": ["beef", "dipping sauce"],
+    "description": "Soggy bread and meat that tastes good."
+}
+}]
+
+//desserts
+"desserts": [{
+    "ice cream": "vanilla",
+    "ingredients": ["heavy cream", "sugar", "vanilla extract", "eggs"],
+    "description": "Cold fat and sugar."
+},
+{
+    "smores": "normal smores",
+    "ingredients": ["graham crackers", "chocolate", "marshmellow"],
+    "description": "Hot, sticky sugar."
+}
+
+]
+
+//drinks
+"drinks": [{
+    "water": "plain water",
+    "ingredients": ["hydrogen", "oxygen"],
+    "description": "Like drinking air."
+
+}
+{
+    "chocolate milk": "you heard me",
+    "ingredients": ["cow juice", "chcolate"],
+    "description": "Sugary cow juice."
+}
+]
+
+
+
