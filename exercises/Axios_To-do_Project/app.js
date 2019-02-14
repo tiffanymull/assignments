fa9411a385e6586de263b8555b5105a9c48bed52
Tiffany Mull <tgwenmull@gmail.com>
2019-02-14 01:13:33 -0700

    stressed

--- exercises/Axios_To-do_Project/app.js
+++ exercises/Axios_To-do_Project/app.js
@@ -1,74 +1,59 @@
-// var axios = require("axios"); <--don't need this bc not using node. 
-        //the script source on html takes care of this.
+var completedToDo = {
+    completed: true,
+}
+var incompleteToDo = {  //<--to update database???
+    completed: false,
+}
+
+
+axios.get("https://api.vschool.io/Tiffany/todo").then(function(response){
+var toDoArray = response.data
+for(var i = 0; i < toDoArray.length; i++){
+
+   var todoContainer = document.createElement("div") //<--anything attached to this as a child will appear bc 
+   var input = document.createElement("input")          //it is attached to get element below
+   var title = document.createElement("h3")
+   var image = document.createElement("img")
+   input.type = "checkbox"
+
+   input.addEventListener("click", function(e) {
+        e.target.parentNode.childNodes[1].classList.toggle("struck")  //<--undoes
+        var toDoID = e.target.parentNode.id
+        axios.get("https://api.vschool.io/Tiffany/todo/" + toDoID).then(function(response){
+            var storedIDs = response.data
+            if (storedIDs.completed === true){  //<-- to update database? axios.put?
+                axios.put("https://api.vschool.io/Tiffany/todo/" + toDoID, JSON.stringify(incompleteToDo))
+            }else{
+                axios.put("https://api.vschool.io/Tiffany/todo/" + toDoID, JSON.stringify(completedToDo))
+
+            }
+        })
+        //    e.target.parentNode.childNodes[1].style.textDecoration = "line-through"; //<--perm. strike
+        }, true)
 
 
 
-// axios.get('https://api.vschool.io/Tiffany/todo').then(function(response){
-//     // console.log(response.data)
-//     for (var i = 0; i < response.data.length; i++) {
-//         console.log(response.data[i].title);
-//         console.log(response.data[i].description);
-//     }
-// })
+       if(toDoArray[i].completed){
+           todoContainer.classList.toggle("struck");
+           input.checked = true;
+       }
 
-axios.get('https://api.vschool.io/Tiffany/todo').then(function(response) {
-    renderToDos(response.data)  //<--array of todos
+       image.src=toDoArray[i].imgUrl
+    //    if(toDOArray[i].imgURL)
+
+       title.textContent = toDoArray[i].title  //<--title up there
+
+       todoContainer.id=toDoArray[i]._id  //<--So from container can look up id of actual todo object. Id attached to parent node.
+
+       todoContainer.appendChild(input)   //<--down here is where it's attached.
+       todoContainer.appendChild(title)
+       todoContainer.appendChild(image)
+       document.getElementById("toDoList").appendChild(todoContainer)  //<--this makes div in html appear
+    // var currentDiv = document.getElementById("toDoList"); 
+    // document.body.insertBefore(todoContainer, currentDiv); 
+   }
 })
-    
-function renderToDos(toDoArray) {            //<--toDoArray is pointing at what response.data was earlier
-    toDoArray.forEach(function(todo){
-        var parent = document.createElement("div");
-        parent.className = "todo";
-        var text = document.createTextNode(todo.title);
-        parent.appendChild(text);
 
-        var input = document.createElement("input");
-        input.className = "input";
-
-        input.type = "checkbox";
-
-        input.addEventListener("click", handleChecked)  //<-- this is what needs to make strike happen.
-
-        parent.appendChild(input);
-
-        addStrikeThrough(parent, todo.completed, input) //<-- does this need to be parent.completed?
-                                                        //<-- or does this need to be isCompleted?
-        document.getElementById("todo-list").appendChild(parent);
-    })
-
-
+function renderChecked(e){
+e.target.parentNode.classList.toggle("nostrike")
 }
-
-function handleChecked(){
-    
-    // for (var i = 0; i < input.length; i++){
-    //     if (input[i].checked){
-    //         addStrikeThrough.push(checkboxes[i].value);
-    //     }
-    // }
-    //do i need a for loop here for input[i]?
-    //does toggle come into play?
-    
-    console.log("checked")
-
-}
-
-function addStrikeThrough(item, isCompleted, input){
-    if(isCompleted){
-        item.style.textDecoration = "line-through";
-        input.checked = true
-    }
-}
-
-
-
-
-
-// var parent = document.createElement("div");
-//         parent.className = "todo";
-//         var text = document.createTextNode(todo.title);
-//         todo.appendChild(text);
-        
-//         document.getElementById("todo-list").appendChild(parent)
-//     })
-// })
