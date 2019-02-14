17dc2bb2d75cb25e0cc07844e4f73da3c0d26285
Tiffany Mull <tgwenmull@gmail.com>
2019-02-14 16:07:54 -0700

    aaaaaarrrrgh

--- exercises/Axios_To-do_Project/app.js
+++ exercises/Axios_To-do_Project/app.js
@@ -1,45 +1,63 @@
-var completedToDo = {
-    completed: true,
-}
-var incompleteToDo = {  //<--to update database???
-    completed: false,
-}
-
-
 axios.get("https://api.vschool.io/Tiffany/todo").then(function(response){
-var toDoArray = response.data
-for(var i = 0; i < toDoArray.length; i++){
+    var toDoArray = response.data
+    for(var i = 0; i < toDoArray.length; i++){
 
-   var todoContainer = document.createElement("div") //<--anything attached to this as a child will appear bc 
-   var input = document.createElement("input")          //it is attached to get element below
-   var title = document.createElement("h3")
-   var image = document.createElement("img")
-   input.type = "checkbox"
+    var todoContainer = document.createElement("div") //<--anything attached to this as a child will appear bc 
+    var input = document.createElement("input")          //it is attached to get element below
+    var title = document.createElement("h3")
+    var image = document.createElement("img")
+        // var newToDo = {                 //<--trying to let the user update api from website. Post? Makes it disappear.
+        //         title: bluePrintTitle,
+        //         description: bluePrintDescription,
+        //         price: bluePrintPrice,
+        //         imgUrl: bluePrintImage,
+        //         Completed: bluePrintCompleted
+        //         };
+    
+        input.type = "checkbox"
 
-   input.addEventListener("click", function(e) {
-        e.target.parentNode.childNodes[1].classList.toggle("struck")  //<--undoes
-        var toDoID = e.target.parentNode.id
-        axios.get("https://api.vschool.io/Tiffany/todo/" + toDoID).then(function(response){
-            var storedIDs = response.data
-            if (storedIDs.completed === true){  //<-- to update database? axios.put?
-                axios.put("https://api.vschool.io/Tiffany/todo/" + toDoID, JSON.stringify(incompleteToDo))
-            }else{
-                axios.put("https://api.vschool.io/Tiffany/todo/" + toDoID, JSON.stringify(completedToDo))
+        if(toDoArray[i].completed){
+        //    todoContainer.style.textDecoration = "line-through"; //<--not undoing line through
+            title.style.textDecoration = "line-through";
+            input.checked = true;
+        }
 
-            }
+        input.addEventListener("click", function(e) {
+            e.target.parentNode.childNodes[1].classList.toggle("struck")  //<--undoes
+            var toDoID = e.target.parentNode.id
+            console.log(e.target.checked)
+            axios.put("https://api.vschool.io/Tiffany/todo/" + toDoID, {completed: e.target.checked}).then(function(response){
+                console.log(e.target.completed)
+                if (e.target.checked === true){
+                    e.target.parentNode.childNodes[1].style.textDecoration = "line-through"; //<--css names dashed camel case in JS
+                }else{
+                    e.target.parentNode.childNodes[1].style.textDecoration = "none";
+                }
+            }).catch(function(error){
+                console.log(error)
+            })
+            
         })
-        //    e.target.parentNode.childNodes[1].style.textDecoration = "line-through"; //<--perm. strike
-        }, true)
+
+        //         var newToDo = {                 //<--trying to let the user update api from website. Post? Makes it disappear.
+        //         title: bluePrintTitle,
+        //         description: bluePrintDescription,
+        //         price: bluePrintPrice,
+        //         imgUrl: bluePrintImage,
+        //         Completed: bluePrintCompleted
+        //         };
 
 
+        // axios.post("https://api.vschool.io/Tiffany/todo/", newTodo).then(function(response){  //<--trying to post
+        //     console.log(response.data);
+        //   }).catch(function(error){
+        //     console.log(error)
+        //   });
+        
 
-       if(toDoArray[i].completed){
-           todoContainer.classList.toggle("struck");
-           input.checked = true;
-       }
+      
 
        image.src=toDoArray[i].imgUrl
-    //    if(toDOArray[i].imgURL)
 
        title.textContent = toDoArray[i].title  //<--title up there
 
@@ -54,6 +72,4 @@ for(var i = 0; i < toDoArray.length; i++){
    }
 })
 
-function renderChecked(e){
-e.target.parentNode.classList.toggle("nostrike")
-}
+//no strike was not necessary
