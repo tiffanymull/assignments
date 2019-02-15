b1fac9f3e61d9a68e60e123e296a8304435b346f
Tiffany Mull <tgwenmull@gmail.com>
2019-02-14 17:01:41 -0700

    AAAAAARRRRGGGHHH

--- exercises/Axios_To-do_Project/app.js
+++ exercises/Axios_To-do_Project/app.js
@@ -6,13 +6,7 @@ axios.get("https://api.vschool.io/Tiffany/todo").then(function(response){
     var input = document.createElement("input")          //it is attached to get element below
     var title = document.createElement("h3")
     var image = document.createElement("img")
-        // var newToDo = {                 //<--trying to let the user update api from website. Post? Makes it disappear.
-        //         title: bluePrintTitle,
-        //         description: bluePrintDescription,
-        //         price: bluePrintPrice,
-        //         imgUrl: bluePrintImage,
-        //         Completed: bluePrintCompleted
-        //         };
+    // var newToDo = document.createElement("input2")  //<--trying to let the user post
     
         input.type = "checkbox"
 
@@ -38,31 +32,14 @@ axios.get("https://api.vschool.io/Tiffany/todo").then(function(response){
             })
             
         })
-
-        //         var newToDo = {                 //<--trying to let the user update api from website. Post? Makes it disappear.
-        //         title: bluePrintTitle,
-        //         description: bluePrintDescription,
-        //         price: bluePrintPrice,
-        //         imgUrl: bluePrintImage,
-        //         Completed: bluePrintCompleted
-        //         };
-
-
-        // axios.post("https://api.vschool.io/Tiffany/todo/", newTodo).then(function(response){  //<--trying to post
-        //     console.log(response.data);
-        //   }).catch(function(error){
-        //     console.log(error)
-        //   });
-        
-
-      
-
+         
        image.src=toDoArray[i].imgUrl
 
        title.textContent = toDoArray[i].title  //<--title up there
 
        todoContainer.id=toDoArray[i]._id  //<--So from container can look up id of actual todo object. Id attached to parent node.
 
+    //    todoContainer.appendChild(input2)
        todoContainer.appendChild(input)   //<--down here is where it's attached.
        todoContainer.appendChild(title)
        todoContainer.appendChild(image)
@@ -71,5 +48,20 @@ axios.get("https://api.vschool.io/Tiffany/todo").then(function(response){
     // document.body.insertBefore(todoContainer, currentDiv); 
    }
 })
+        document.form.addEventListener("submit", function(e){           //<--this is maybe the missing piece for adding new post
+            e.preventDefault();
+
+            newTodo = {
+                title: document.form.title.value,
+                description: document.form.description.value,
+                imgUrl: document.form.image.value
+            }
+            axios.post("https://api.vschool.io/Tiffany/todo", newTodo).then(function(response){
+                console.log(response.data);
+            
+            })
+        })
+
+
 
 //no strike was not necessary
