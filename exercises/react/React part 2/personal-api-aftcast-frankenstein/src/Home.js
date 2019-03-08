1b4d7a7d4fe9adf8713fb07dbbbc6d968afa7ee0
Tiffany Mull <tgwenmull@gmail.com>
2019-03-08 10:12:33 -0700

    added images and made them responsive

--- exercises/react/React part 2/personal-api-aftcast-frankenstein/src/Home.js
+++ exercises/react/React part 2/personal-api-aftcast-frankenstein/src/Home.js
@@ -5,10 +5,13 @@ const Home = () => {
         <div className="wrapper">
             <div className="homeContent">
                 <h1>Home</h1>
-                <p>Welcome to Aftcasts, where you can obsess about the climes of other times.  
-                    Most people are interested in the weather of yesteryear, but if you’re a 
-                    freak who likes anticipating rainstorms, we won’t judge.</p>
-                <a href="https://www.youtube.com/watch?v=UftUkRcRC_Q">Strange Weather</a>
+                {/* Not using pageIntro yet */}
+                  <div className="pageIntro">
+                    <p>Welcome to Aftcasts, where you can obsess about the climes of other times.  
+                        Most people are interested in the weather of yesteryear, but if you’re a 
+                        freak who likes anticipating rainstorms, we won’t judge.</p>
+                    <a href="https://www.youtube.com/watch?v=UftUkRcRC_Q">Strange Weather</a>
+                  </div>
             </div>
         </div>
     )
--- exercises/react/React part 2/personal-api-aftcast-frankenstein/src/Tips.js
+++ exercises/react/React part 2/personal-api-aftcast-frankenstein/src/Tips.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import crying from './crying_lady.jpg'
+import ant from './cropped-ant.jpg'
+import arrgh from './aargh-old.jpg'
 
 const Tips = () => {
     return (
@@ -6,6 +9,16 @@ const Tips = () => {
             <div className="tipsContent">
                 <h1>Tips</h1>
                 <p>Are you looking for advice on how best to enjoy the weather you may or may not have experienced?  Of course you are!  Here are some suggestions:</p>
+                <img src={crying} alt="crying lady" />
+                <p>Make your tears match the rain you could've soaked up.</p> <br></br><br></br>
+                <img src={ant} alt="sad little ant" />
+                <p>Wonder where the ants took shelter that winter.</p> <br></br><br></br>
+                <img src={arrgh} alt="creepy straining" />
+                <p>Obsess.</p> <br></br><br></br>
+            </div> <br></br>
+
+            <div className="tipsImage1">
+
             </div>
         </div>
     )
