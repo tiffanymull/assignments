be789f6448fba74655c29bff63d97eff5a3fbc4b
Tiffany Mull <tgwenmull@gmail.com>
2019-03-12 14:28:33 -0600

    starting todo express

---
+++ exercises/Node/todo-express-with-node/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const uuid = require('uuid')
+const port = 7777;
+
+const database = [
+    {
+        name: '',
+        ancestry: '',
+        weapon: '',
+        _id: 
+    }
+]
