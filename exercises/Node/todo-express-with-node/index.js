d45d48be507e7e331a2d6e7d7c959a4b496d946d
Tiffany Mull <tgwenmull@gmail.com>
2019-03-12 18:21:28 -0600

    .

--- exercises/Node/todo-express-with-node/index.js
+++ exercises/Node/todo-express-with-node/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-// const uuid = require('uuid')
+const uuid = require('uuid')
 const port = 7777;
+
+app.use(express.json()) //<--makes body manifest. must make use of. 
 
 //had to rename this to index.js bc package.json references that. 
 
@@ -29,11 +31,21 @@ const database = [
     },
 ]
 
-app.get('/lotr', (req, res)=> {
+app.get('/lotr', (req, res) => {
     res.send(database)
+})
+
+app.post('/lotr', (req, res) => {
+    // console.log(req)
+    const newObj = req.body;  //<- creating a new object so you can post
+    //    console.log(req.body)
+    newObj._id = uuid.v4();  //<-give it an id using the package installed
+    database.push(newObj)        //<-so that it gets added to the database
+    res.send(newObj)                //<--works now. just leaves when refresh bc not saving to actual database
 })
 
 app.listen(port, () => {
     console.log(`server is running ${port}`) //<-this is what makes localhost available to, like, postman
 })
 
+app.put
